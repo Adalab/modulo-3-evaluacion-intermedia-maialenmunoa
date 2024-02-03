3dca3c887c3a65130b49ac1adae1557051326d6f
maialenmunoa <maialenmunoa@gmail.com>
2024-02-03 09:34:42 +0100

    add name props

--- src/components/QuoteItem.jsx
+++ src/components/QuoteItem.jsx
@@ -1,11 +1,17 @@
-function QuoteItem() {
+import PropTypes from "prop-types";
+
+function QuoteItem({name}) {
 
     return (
         <>
-            <h2>Monica</h2>
+            <h2>{name}</h2>
             <p>¡Bienvenidos a la vida real! No es tan divertida como la televisión, pero a veces consigues un trabajo y te enamoras. Pero entonces, algo malo pasa. Como cuando te das cuenta de que todos tus calcetines tienen agujeros. ¿Dónde se van todos los calcetines?</p>
         </>
     );
-  }
+}
+
+QuoteItem.propTypes = {
+    name: PropTypes.string.isRequired,
+};  
   
-  export default QuoteItem;
+export default QuoteItem;
--- src/components/QuotesList.jsx
+++ src/components/QuotesList.jsx
@@ -5,26 +5,26 @@ function QuotesList() {
     return (
         <ul className="characters">
         <li>
-          <QuoteItem />
+          <QuoteItem name="Monica" />
         </li>
         <li>
-          <h2>Ross</h2>
+          <QuoteItem name="Ross" />
           <p>Estamos en una pausa.</p>
         </li>
         <li>
-          <h2>Rachel</h2>
+          <QuoteItem name="Rachel" />
           <p>¡No estoy gorda! Estoy embarazada.</p>
         </li>
         <li>
-          <h2>Chandler</h2>
+          <QuoteItem name="Chandler" />
           <p>¡Pivotar!</p>
         </li>
         <li>
-          <h2>Joey</h2>
+          <QuoteItem name="Joey" />
           <p>¿Cómo estás tú haciendo?</p>
         </li>
         <li>
-          <h2>Phoebe</h2>
+          <QuoteItem name="Phoebe" />
           <p>¡Oh, Dios mío</p>
         </li>
       </ul>
