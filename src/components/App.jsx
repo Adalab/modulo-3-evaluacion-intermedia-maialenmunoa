cc0df9a8bc667fe01c8d649f850b05b214bb7a56
maialenmunoa <maialenmunoa@gmail.com>
2024-02-03 09:59:19 +0100

    add use state data friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-import '../scss/App.scss';
-
-import dataFriends from '../data/friends.json';
+import { useState } from 'react';
 
 import Filters from './Filters';
 import Header from './Header';
 import QuotesList from './QuotesList';
 
+import '../scss/App.scss';
+
+import dataFriends from '../data/friends.json';
+
 function App() {
+  const [quotes, setQuotes] = useState(dataFriends);
+
   return (
     <div className="page">
       <Header />
